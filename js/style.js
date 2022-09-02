89fa764df1e0f37ee36c91efff729ac484d4b1e3
salahuddin8544 <salahuddinahmed8544@gmail.com>
2022-09-03 00:22:13 +0600

    complete categoy section

--- js/style.js
+++ js/style.js
@@ -1,3 +1,24 @@
-const loadData = () =>{
-    fetch('')
+const loadCatagory = async() =>{
+    const url = `https://openapi.programming-hero.com/api/news/categories`
+    const res = await fetch(url);
+    const data = await res.json();
+   displayCatagory(data.data.news_category);
 }
+const displayCatagory = (categories)=>{
+    console.log(categories);
+    const catagoryContainer = document.getElementById('category-container');
+    categories.forEach(category=>{
+        console.log(category);
+        const div = document.createElement('div')
+        div.innerHTML = `
+        <a onclick="newsDetails(${category.category_id})" href="">${category.category_name}</a>
+        `
+       catagoryContainer.appendChild(div)
+    })
+}
+
+// newDetails news
+const newsDetails = async data =>{
+    console.log(data);
+}
+loadCatagory()
