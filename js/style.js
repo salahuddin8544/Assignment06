356dd9e887cd4c65e39b4d233d287ab2ee61be24
salahuddin8544 <salahuddinahmed8544@gmail.com>
2022-09-03 01:09:28 +0600

    complete category items sections

--- js/style.js
+++ js/style.js
@@ -5,10 +5,8 @@ const loadCatagory = async() =>{
    displayCatagory(data.data.news_category);
 }
 const displayCatagory = (categories)=>{
-    console.log(categories);
     const catagoryContainer = document.getElementById('category-container');
     categories.forEach(category=>{
-        console.log(category);
         const div = document.createElement('div')
         div.innerHTML = `
         <a onclick="newsDetails(${category.category_id})" href="">${category.category_name}</a>
@@ -18,7 +16,16 @@ const displayCatagory = (categories)=>{
 }
 
 // newDetails news
-const newsDetails = async data =>{
+const newsDetails = async id =>{
+    const url = `https://openapi.programming-hero.com/api/news/category/${id}`
+    const res = await fetch(url);
+    const data = await res.json();
     console.log(data);
+    // displayNews(data)
+    // console.log(data);
+}
+const displayNews = news =>{
+     console.log(news);
+    
 }
 loadCatagory()
